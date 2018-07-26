cebe2a256bfbabd2ff4ef336785b79f830b0e712
Jose Barron Tovar <josebarrontovar@hotmail.com>
2018-07-26 16:24:04 -0500

    Initial commit

---
+++ Project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuariosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
